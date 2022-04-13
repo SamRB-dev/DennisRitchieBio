4299e87e4db6b48026d0ff62c97d133c4e45dbae
Fr34K008 <sam_freak008@protonmail.com>
2022-04-13 16:09:15 +0600

    All files

---
+++ js/iconButton.js
@@ -0,0 +1,11 @@
+function allDark(){
+    // ID: "title","bio","short","footer","body", "dev","contact"
+    document.getElementById("title").style.color = "#fff3f2";
+    document.getElementById("bio").style.color = "#fff3f2";
+    document.getElementById("short").style.color = "#fff3f2";
+    document.getElementById("footer").style.color = "#fff3f2";
+    document.getElementById("dev").style.color = "#fff3f2";
+    document.getElementById("contact").style.color = "#fff3f2"
+    document.getElementById("body").style.backgroundColor = "#201f1f";
+    document.getElementById("icon").style.backgroundColor = "#201f1f";
+}
